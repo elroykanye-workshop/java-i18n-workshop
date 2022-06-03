3187434cc63a41ac36a3aa786024f6744a765ab6
Elroy Kanye <elroykdev@gmail.com>
2022-06-03 21:22:56 +0100

    added example with resource bundles

--- s1-resource-bundles/src/main/java/com/elroykanye/javaloc/Main.java
+++ s1-resource-bundles/src/main/java/com/elroykanye/javaloc/Main.java
@@ -1,4 +1,16 @@
 package com.elroykanye.javaloc;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.FRENCH);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("greetings", Locale.getDefault());
+
+        System.out.println(resourceBundle.getString("morning_greeting"));
+
+        Locale.setDefault(Locale.GERMAN);
+        System.out.println(resourceBundle.getString("greeting"));
+    }
 }
