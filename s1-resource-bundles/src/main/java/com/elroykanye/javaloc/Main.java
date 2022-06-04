8fe3835480c25294d1f2f2aaa7fc13e05f2137f1
Elroy Kanye <elroykdev@gmail.com>
2022-06-04 23:25:58 +0100

    add instructions.md

--- s1-resource-bundles/src/main/java/com/elroykanye/javaloc/Main.java
+++ s1-resource-bundles/src/main/java/com/elroykanye/javaloc/Main.java
@@ -1,16 +1,19 @@
 package com.elroykanye.javaloc;
 
+import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main {
     public static void main (String[] args) {
-        Locale.setDefault(Locale.FRENCH);
+
+        Locale.setDefault(Locale.GERMAN);
         ResourceBundle resourceBundle = ResourceBundle.getBundle("greetings", Locale.getDefault());
 
         System.out.println(resourceBundle.getString("morning_greeting"));
+        System.out.println(resourceBundle.getString("afternoon_greeting"));
+        System.out.println(NumberFormat.getInstance().format(300000));
 
-        Locale.setDefault(Locale.GERMAN);
-        System.out.println(resourceBundle.getString("greeting"));
     }
 }
+
